ee65896b1bbcc05074068e59529c37d192073bc1
Brian Ostrovsky <brian@locable.com>
2016-12-07 21:53:55 +0000

    Redo

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,11 +1,12 @@
 class ContactMailer < ActionMailer::Base
-   default to: "brian@locable.com"
-       
-    def contact_email(name, email, body)
-        @name = name
-        @email = email
-        @body = body
-        
-        mail(from: email, subject: 'Contact form message')
-    end       
+  
+  default to: 'your@email_address.com'
+  
+  def contact_email(name, email, body)
+    @name = name
+    @email = email
+    @body = body
+    mail(from: email, subject: 'Contact Form Message')
+  end
+
 end
